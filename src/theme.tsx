e0e9d91020eee82eb35dac60866abcbd0e21e6a1
chokwatcharathep <chok.watcharathep@maqe.com>
2024-05-22 10:21:24 +0700

    chore(config): setup project format via prettierrc

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,9 +1,9 @@
-"use client";
-import { Roboto } from "next/font/google";
-import { createTheme } from "@mui/material/styles";
-import NextLink from "next/link";
-import { forwardRef } from "react";
-import { LinkProps as NextLinkProps } from "next/link";
+'use client';
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+import NextLink from 'next/link';
+import { forwardRef } from 'react';
+import { LinkProps as NextLinkProps } from 'next/link';
 
 const LinkBehavior = forwardRef<HTMLAnchorElement, NextLinkProps>(
   function LinkBehavior(props, ref) {
@@ -12,9 +12,9 @@ const LinkBehavior = forwardRef<HTMLAnchorElement, NextLinkProps>(
 );
 
 const roboto = Roboto({
-  weight: ["300", "400", "500", "700"],
-  subsets: ["latin"],
-  display: "swap",
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 const theme = createTheme({
@@ -23,14 +23,14 @@ const theme = createTheme({
   },
   palette: {
     primary: {
-      main: "#cddf52",
+      main: '#cddf52',
     },
     secondary: {
-      main: "#1b1a16",
+      main: '#1b1a16',
     },
     background: {
-      gray: '#f0ebde'
-    }
+      gray: '#f0ebde',
+    },
   },
   shape: {
     borderRadius: 8,
@@ -39,26 +39,26 @@ const theme = createTheme({
 
 // override component
 theme.components = {
-    MuiLink: {
-      defaultProps: {
-        component: LinkBehavior,
-      },
-      styleOverrides: {
-        root: {
-          textDecoration: 'unset',
+  MuiLink: {
+    defaultProps: {
+      component: LinkBehavior,
+    },
+    styleOverrides: {
+      root: {
+        textDecoration: 'unset',
 
-          ":hover": {
-            color: theme.palette.primary.light
-          }
-        }
-      }
-    },
-    MuiButton: {
-      defaultProps: {
-        disableElevation: true,
-        LinkComponent: LinkBehavior,
+        ':hover': {
+          color: theme.palette.primary.light,
+        },
       },
     },
-}
+  },
+  MuiButton: {
+    defaultProps: {
+      disableElevation: true,
+      LinkComponent: LinkBehavior,
+    },
+  },
+};
 
 export default theme;
