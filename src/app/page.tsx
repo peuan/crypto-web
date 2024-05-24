5398069b25c95508aaf0b26299ddeff090b83737
thasup <thanachonfirst@hotmail.com>
2024-05-25 03:04:46 +0700

    Add coming soon section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import ComingSoonSection from "@/components/ComingSoonSection"
 import HeroSection from "@/components/HeroSection"
-import { Button } from "@mui/material"
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <ComingSoonSection />
     </main>
   )
 }
