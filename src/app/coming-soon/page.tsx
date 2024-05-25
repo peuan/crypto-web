235d24976ff9bc97b64bb15e1caa2faa836c875e
thasup <thanachonfirst@hotmail.com>
2024-05-25 13:30:35 +0700

    fix: Fix coming soon page layout

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -1,17 +1,36 @@
 import { Box, Typography } from "@mui/material";
 import TimeLineIcon from "@mui/icons-material/TimeLine";
+import WrapperContainer from "@/components/WrapperContainer";
 
 const ComingSoonPage = () => {
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      height="100%"
+    <WrapperContainer
+      sx={{
+        bgcolor: "secondary.main",
+        py: 2,
+        minHeight: {
+          xs: "calc(100vh - 64px)",
+          md: "calc(100vh - 84px)",
+          lg: "calc(100vh - 300px)",
+        },
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "background.custom.secondary",
+      }}
     >
-      <TimeLineIcon sx={{ fontSize: 40, mr: 1 }} />
-      <Typography variant="h4">Coming Soon...</Typography>
-    </Box>
+      <Box
+        sx={{
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TimeLineIcon sx={{ fontSize: 40, mr: 1 }} />
+        <Typography variant="h4">Coming Soon...</Typography>
+      </Box>
+    </WrapperContainer>
   );
 };
 
